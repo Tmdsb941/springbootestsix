de9b6ba0400f6c630e4d29e06747d450c307cf2b
Tmdsb941 <1927919399@qq.com>
2018-12-15 17:46:36 +0800

    Initial commit

---
+++ src/main/java/com/cs/springbootestsix/SpringbootestsixApplication.java
@@ -0,0 +1,14 @@
+package com.cs.springbootestsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootestsixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootestsixApplication.class, args);
+    }
+
+}
+
